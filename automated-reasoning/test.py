163352ccecea1d1506becf8703c2120a9f00b0be
PimLeerkes <pimsimon.leerkes@gmail.com>
2024-09-12 10:20:40 +0200

    volgens mij werkt het nu

--- automated-reasoning/test.py
+++ automated-reasoning/test.py
@@ -3,9 +3,6 @@ from z3 import *
 BUSSES = 8
 W_BUS = 8000
 
-# Create variables i = bus
-#B = [Int(f"b_{i}") for i in range(BUSSES)]
-
 SAFFRON = 4
 W_SAFFRON = 700
 S = [[Bool(f"s_{i}_{j}") for j in range(SAFFRON)] for i in range(BUSSES)]
@@ -25,45 +22,79 @@ P = [[Bool(f"p_{i}_{j}") for j in range(PEARS)] for i in range(BUSSES)]
 #bus_weight_limit = [B[i] <= W_BUS for i in range(BUSSES)]
 
 W_APPLES = 400
+A = [Int(f"a_{i}") for i in range(BUSSES)]
 
 def smaller_pairs(n, m):
     return [(i, k) for i in range(m) for k in range(m) if n <= i < k < m]
 
-def saffron_sum(i, max_weight, amount):
+def saffron_sum(i):
     """Get the summed weight of saffron for truck i."""
     return Sum([S[i][j] * W_SAFFRON for j in range(SAFFRON)])
 
 def mushroom_sum(i):
     """Get the summed weight of mushroom for truck i."""
-    return Sum([S[i][j] * W_MUSHROOMS for j in range(MUSHROOMS)])
+    return Sum([M[i][j] * W_MUSHROOMS for j in range(MUSHROOMS)]) if i < 3 else 0
 
 def goat_sum(i):
     """Get the summed weight of mushroom for truck i."""
-    return Sum([S[i][j] * W_GOATS for j in range(GOATS)])
+    return Sum([G[i][j] * W_GOATS for j in range(GOATS)])
 
 def pear_sum(i):
     """Get the summed weight of mushroom for truck i."""
-    return Sum([S[i][j] * W_PEARS for j in range(GOATS)])
+    return Sum([P[i][j] * W_PEARS for j in range(GOATS)])
 
-mushrooms = [Or([M[i][j] for i in range(3)]) for j in range(BUSSES)]
-goats = [Or([G[i][j] for i in range(BUSSES)]) for j in range(GOATS)]
-pears = [Or([P[i][j] for i in range(BUSSES)]) for j in range(PEARS)]
 
 all_saffron_in_at_least_one_bus = [Or([S[i][j] for i in range(BUSSES)]) for j in range(SAFFRON)]
-no_two_busses_have_the_same_saffron = [
+no_one_saffron_in_two_busses = [
     And([
         Or([Not(S[i][j]), Not(S[k][j])]) 
     for (i,k) in smaller_pairs(0,8)]) 
 for j in range(SAFFRON)]
 
-no_truck_weight_exceeded = [
-    Sum([saffron_sum(i), mushroom_sum(i), goat_sum(i), pear_sum(i)]) <= W_BUS
+all_mushrooms_in_at_least_one_bus = [Or([M[i][j] for i in range(3)]) for j in range(BUSSES)]
+no_one_mushroom_in_two_busses = [
+    And([
+        Or([Not(M[i][j]), Not(M[k][j])]) 
+    for (i,k) in smaller_pairs(0,3)]) 
+for j in range(MUSHROOMS)]
+
+all_goats_in_at_least_one_bus = [Or([G[i][j] for i in range(BUSSES)]) for j in range(GOATS)]
+no_one_goat_in_two_busses = [
+    And([
+        Or([Not(G[i][j]), Not(G[k][j])]) 
+    for (i,k) in smaller_pairs(0,8)]) 
+for j in range(GOATS)]
+
+all_pears_in_at_least_one_bus = [Or([P[i][j] for i in range(BUSSES)]) for j in range(PEARS)]
+no_one_pear_in_two_busses = [
+    And([
+        Or([Not(P[i][j]), Not(P[k][j])]) 
+    for (i,k) in smaller_pairs(0,8)]) 
+for j in range(PEARS)]
+
+
+at_most_one_saffron_in_a_bus = [
+    And([
+        Or([Not(S[i][k]), Not(S[i][j])]) 
+    for (k,j) in smaller_pairs(0,4)]) 
 for i in range(BUSSES)]
 
-#max_one_saffron_per_bus = [And(Or(Not(s[i][0]),s[][]),s[i][1],s[i][2],s[i][3]) for i in range(BUSSES)]
-#saffron_max_one_bus = [And([Or(Not(Si][j])m,Not(S[k][j])) for j in range(BUSSES) if 0 < i < k <= 8]) for i in range SAFFRON]
-#[And(B[i] <= 8000], B[i] == Sum(Sum([S[i][j]*700 for j in range(SAFFRON)]),Sum([M[i][j]*1000 for j in range(MUSHROOMS)]),Sum([G[i][j]*2500 for j in range(GOATS)]),Sum([P[i][j]*400 for j in range(PEARS)])))for i in range(BUSSES)]
 
-phi = 
-print(phi)
-solve(phi)
+no_truck_weight_exceeded = [
+    And(Sum(saffron_sum(i), mushroom_sum(i), goat_sum(i), pear_sum(i), A[i]*W_APPLES) <= W_BUS, A[i] >= 0)
+for i in range(BUSSES)]
+
+
+minimum_amount_of_apples = [Sum([A[i] for i in range(BUSSES)]) > 59]
+
+phi = all_saffron_in_at_least_one_bus + no_one_saffron_in_two_busses + all_mushrooms_in_at_least_one_bus + no_one_mushroom_in_two_busses + all_goats_in_at_least_one_bus + no_one_goat_in_two_busses + all_pears_in_at_least_one_bus + no_one_pear_in_two_busses + no_truck_weight_exceeded + at_most_one_saffron_in_a_bus + minimum_amount_of_apples
+#print(phi)
+
+s = Solver()
+sadd(phi)
+s.check()
+m = s.model()
+total_apples = Sum([m.evaluate(A[i]) for i in range(BUSSES)])
+print(total_apples)
+
+#solve(phi)
