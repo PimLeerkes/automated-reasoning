38ded1134a3968b677fc702a2768e09f1d452127
PimLeerkes <pimsimon.leerkes@gmail.com>
2024-09-18 14:41:24 +0200

    verder

--- automated-reasoning/exercise3.py
+++ automated-reasoning/exercise3.py
@@ -5,20 +5,24 @@ PEOPLE = 10
 PEOPLE_PER_ROUND = 5
 
 
-P = [Int(f"p_{i}")for i in range(PEOPLE)]
-R_a = [Int(f"R_a_{i}")for i in range(ROUNDS)]
-R_b = [Int(f"R_b_{i}")for i in range(ROUNDS)]
+P = [Int(f"p_{i}") for i in range(PEOPLE)]
+
+R_a = [Int(f"r_a_{i}") for i in range(ROUNDS)]
+R_b = [Int(f"r_b_{i}") for i in range(ROUNDS)]
+
+PR = [[Int(f"p_{i}_{j}") for i in range(PEOPLE)] for j in range(ROUNDS)]
+
 
 #We make the couples:
 for i in range(COUPLES):
-    P[i*2] = i
-    P[i*2 + 1] = i
+    P[i*2] = Int(i)
+    P[i*2 + 1] = Int(i)
 
 each_round_in_two_houses = [And(Not(R_a[i] == R_b[i]), Or(R_a[i] == 0, R_a[i] == 1, R_a[i] == 2, R_a[i] == 3, R_a[i] == 4), Or(R_b[i] == 0, R_b[i] == 1, R_b[i] == 2, R_b[i] == 3, R_b[i] == 4)) for i in range(ROUNDS)]
 
-each_round_has_five_people_per_house = [And() for i in range(ROUNDS)]
+each_round_has_five_people_per_house = [And(Sum(PR[i]) == 5, And[Or(PR[i][j] == 0, PR[i][j] == 1) for j in range(PEOPLE)]) for i in range(ROUNDS)]
 
 
-phi = each_round_in_two_houses
+phi = each_round_in_two_houses + each_round_has_five_people_per_house
 
 solve(phi)
