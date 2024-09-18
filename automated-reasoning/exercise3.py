bcb0dd1660a1383effb758cd7435d7087dd79b6e
PimLeerkes <pimsimon.leerkes@gmail.com>
2024-09-18 14:43:40 +0200

    .

--- automated-reasoning/exercise3.py
+++ automated-reasoning/exercise3.py
@@ -21,7 +21,7 @@ for i in range(COUPLES):
 
 each_round_in_two_houses = [And(Not(R_a[i] == R_b[i]), Or(R_a[i] == 0, R_a[i] == 1, R_a[i] == 2, R_a[i] == 3, R_a[i] == 4), Or(R_b[i] == 0, R_b[i] == 1, R_b[i] == 2, R_b[i] == 3, R_b[i] == 4)) for i in range(ROUNDS)]
 
-each_round_has_five_people_per_house = [And(Sum(PR[i]) == 5, And[Or(PR[i][j] == 0, PR[i][j] == 1) for j in range(PEOPLE)]) for i in range(ROUNDS)]
+each_round_has_five_people_per_house = [And(Sum(PR[i]) == 5, And([Or(PR[i][j] == 0, PR[i][j] == 1) for j in range(PEOPLE)])) for i in range(ROUNDS)]
 
 
 phi = each_round_in_two_houses + each_round_has_five_people_per_house
