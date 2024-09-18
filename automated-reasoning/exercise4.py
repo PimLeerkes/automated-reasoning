c6fcc593c382b252934782c5a0b9c6231e51b958
PimLeerkes <pimsimon.leerkes@gmail.com>
2024-09-18 17:56:26 +0200

    4.1 is opgelost

--- automated-reasoning/exercise4.py
+++ automated-reasoning/exercise4.py
@@ -2,8 +2,9 @@ from z3 import *
 
 ITERATIONS = 10
 
-a = [Int(f"a_{i}")for i in range(ITERATIONS+1)]
-b = [Int(f"b_{i}")for i in range(ITERATIONS+1)]
+a = [Int(f"a_{i}") for i in range(ITERATIONS+1)]
+b = [Int(f"b_{i}") for i in range(ITERATIONS+1)]
+
 
 #the preconditions:
 preconditions = And(a[0] == 1, b[0] == 1)
@@ -14,9 +15,9 @@ forloop = And([Or(And(a[i] == a[i-1] + 2*b[i-1], b[i] == b[i-1] + i), And(a[i] =
 #forloop = Or(And([And(a[i] == a[i-1] + 2*b[i-1], b[i] == b[i-1] + i) for i in range(1,ITERATIONS+1)]), And([And(a[i] == a[i-1] + i, b[i] == b[i-1] + a[i]) for i in range(1,ITERATIONS+1)]))
 
 #the post condition
-postcondition = Not(b[ITERATIONS-1] == 700 + 10)
+postcondition = b[ITERATIONS-1] == 700 + 10
 
-#for all values of n = 1,2,...,10 it will never reach crash?
+#it will not reach crash for n=1, 3, 5, 8, 10
 
 phi = And(preconditions, forloop, postcondition)
 
