82e2aee4f1502dedeb709b878222e6a1ca2935b9
YouGuessedMyName <ivo@melse.info>
2024-09-19 01:26:03 +0200

    rename from paranoia

--- automated-reasoning/exercise3_new.py
+++ automated-reasoning/exercise3_new.py
@@ -21,9 +21,9 @@ def invert_2d_list(l):
 
 V_people_locations = [[Int(f"p{p}_r{r}_c") for r in range(ROUNDS)] for p in range(PPL)]
 V_people_locations_inverted = invert_2d_list(V_people_locations)
-F_people_locations_bound = [And([And(loc >= 0, loc < HOUSES) for loc in p]) for p in V_people_locations]
+F1_people_locations_bound = [And([And(loc >= 0, loc < HOUSES) for loc in p]) for p in V_people_locations]
 V_people_couples = [Int(f"p{p}_c") for p in range(PPL)]
-F_people_couples_bound = [And(couple >= 0, couple < COUPLES) for couple in V_people_couples]
+F2_people_couples_bound = [And(couple >= 0, couple < COUPLES) for couple in V_people_couples]
 
 # Each couple consists of two people living together in one house, so there are five houses in total. 
 def z3_count(x:int, l:list[ArithRef]):
@@ -34,24 +34,24 @@ def z3_count(x:int, l:list[ArithRef]):
 def flatten(xss):
     return [x for xs in xss for x in xs]
 
-F_couple_is_two_people = [z3_count(x, V_people_couples) == 2 for x in range(COUPLES)]
+F3_couple_is_two_people = [z3_count(x, V_people_couples) == 2 for x in range(COUPLES)]
 
 # The dinner consists of five rounds. 
 
 V_round_couples_a = [Int(f"r{r}_ca") for r in range(ROUNDS)]
 V_round_couples_b = [Int(f"r{r}_cb") for r in range(ROUNDS)]
 V_round_couples = V_round_couples_a + V_round_couples_b
-F_round_couples_bound = [And(0 <= rc, rc < COUPLES) for rc in (V_round_couples)]
+F4_round_couples_bound = [And(0 <= rc, rc < COUPLES) for rc in (V_round_couples)]
 
 # Each round is held in two houses at the same time.
-F_each_round_two_houses = [rca != rcb for (rca, rcb) in zip(V_round_couples_a, V_round_couples_b)]
+F5_each_round_two_houses = [rca != rcb for (rca, rcb) in zip(V_round_couples_a, V_round_couples_b)]
 
 # , with five people in each house(/couple).
 def num_people_in_house_at_round(r:int, hc:int):
     ppl_locs = V_people_locations_inverted[r]
     return z3_count(hc, ppl_locs)
 
-F_five_people_in_each_house_every_round = [
+F6_five_people_in_each_house_every_round = [
     And([
         Implies(V_round_couples_a[r_no] == c_no, num_people_in_house_at_round(r_no, c_no) == 5),
         Implies(V_round_couples_b[r_no] == c_no, num_people_in_house_at_round(r_no, c_no) == 5),
@@ -61,14 +61,14 @@ F_five_people_in_each_house_every_round = [
 
 # Every couple hosts two rounds in their house, 
 
-F_every_couple_two_rounds = [
+F7_every_couple_two_rounds = [
     z3_count(c_no, V_round_couples) == 2
     for c_no in range(COUPLES)
 ]
 
 # for which both hosts have to be present.
 
-F_couple_present_at_own_party = [
+F8_couple_present_at_own_party = [
     Implies(And(V_round_couples[r_no] == c_no, V_people_couples[p_no] == c_no), 
             V_people_locations[p_no][r_no] == c_no)
     for r_no in range(ROUNDS) 
@@ -79,7 +79,7 @@ F_couple_present_at_own_party = [
 
 # No participant may be in the same house with another participant for all five rounds. 
 
-F_no_same_house_always = [
+F9_no_same_house_always = [
     Not(And([
         V_people_locations[p1_no][r_no] == V_people_locations[p2_no][r_no] for r_no in range(ROUNDS)
     ]))
@@ -137,15 +137,15 @@ F_BCD = F_B_meet_each_other_at_most_thrice + F_C_couples_never_meet_outside_own_
 ###
 
 # ONLY V HERE
-phi = F_people_locations_bound +\
-F_people_couples_bound +\
-F_couple_is_two_people +\
-F_round_couples_bound +\
-F_each_round_two_houses +\
-F_five_people_in_each_house_every_round +\
-F_every_couple_two_rounds +\
-F_couple_present_at_own_party +\
-F_no_same_house_always +\
+phi = F1_people_locations_bound +\
+F2_people_couples_bound +\
+F3_couple_is_two_people +\
+F4_round_couples_bound +\
+F5_each_round_two_houses +\
+F6_five_people_in_each_house_every_round +\
+F7_every_couple_two_rounds +\
+F8_couple_present_at_own_party +\
+F9_no_same_house_always +\
 F_AD # Change this per question
 
 
