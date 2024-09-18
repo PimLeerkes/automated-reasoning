f6d86ebfd76eb57ffdecbdd4d87a9c16bf0f1693
YouGuessedMyName <ivo@melse.info>
2024-09-18 14:18:17 +0200

    printer bug fix

--- automated-reasoning/exercise3.py
+++ automated-reasoning/exercise3.py
@@ -1,4 +1,5 @@
 from z3 import *
+from pretty_printer import solve_and_print
 
 HOUSES, COUPLES, ROUNDS = 5, 5, 5
 PEOPLE = 10
@@ -10,9 +11,9 @@ R_a = [Int(f"R_a_{i}")for i in range(ROUNDS)]
 R_b = [Int(f"R_b_{i}")for i in range(ROUNDS)]
 
 #We make the couples:
-for i in range(COUPLES):
-    P[i*2] = i
-    P[i*2 + 1] = i
+# for i in range(COUPLES):
+#     P[i*2] = i
+#     P[i*2 + 1] = i
 
 each_round_in_two_houses = [And(Not(R_a[i] == R_b[i]), Or(R_a[i] == 0, R_a[i] == 1, R_a[i] == 2, R_a[i] == 3, R_a[i] == 4), Or(R_b[i] == 0, R_b[i] == 1, R_b[i] == 2, R_b[i] == 3, R_b[i] == 4)) for i in range(ROUNDS)]
 
@@ -20,5 +21,4 @@ each_round_has_five_people_per_house = [And() for i in range(ROUNDS)]
 
 
 phi = each_round_in_two_houses
-
-solve(phi)
+solve_and_print(P, R_a, R_b, all_vars=True)
--- automated-reasoning/pretty_printer.py
+++ automated-reasoning/pretty_printer.py
@@ -6,13 +6,14 @@ def solve_and_print(P, R_a, R_b, all_vars=False, pretty=True):
     print("Solving")
     s.check()
     m = s.model()
+    print(phi)
     results = {var: m.evaluate(var) for var in phi}
     if all_vars:
         print(results)
     if pretty:
         print("===\nHOUSES and COUPLES")
         for i in range(5):
-            out = "House {i}: "
+            out = f"House {i}: "
             for p in P:
                 val = results[p]
                 if val == i:
@@ -22,7 +23,7 @@ def solve_and_print(P, R_a, R_b, all_vars=False, pretty=True):
         for i, (r_a, r_b) in enumerate(zip(R_a, R_b)):
             print("======")
             print(f"ROUND: {i}")
-            print(f"House a: {results(r_a)}")
-            print(f"House b: {results(r_b)}")
+            print(f"House a: {results[r_a]}")
+            print(f"House b: {results[r_b]}")
             print("People: TODO")
     
