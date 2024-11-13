2c7c6313da9ae8196e83b8086d770e113723fd50
PimLeerkes <pimsimon.leerkes@gmail.com>
2024-11-13 14:01:28 +0100

    mastermind part b now works

--- automated-reasoning/automated_reasoning/assignment2/mastermind/mastermind.py
+++ automated-reasoning/automated_reasoning/assignment2/mastermind/mastermind.py
@@ -69,36 +69,52 @@ S_constraint = And([And(0<s,s<=K) for s in S])
 #for a we don't want duplicates in the solution
 no_duplicates = And([And([Implies(i != j, S[i] != S[j]) for i in range(len(S))]) for j in range(len(S))])
 
-
-#correct colors:
+#correct colors (same for a and b):
 def correct(guess: list[int], n: int):
-  return Sum([If(S[i] == get_list(guess)[i],1,0) for i in range(N)]) == n
-  #return Or([And(len(I) == n, And([S[i] == get_list(guess)[i] for i in I]))
-  #              for I in powerset([n for n in range(N)])])
+  return Sum([If(S[i] == get_list(guess)[i], 1, 0) for i in range(N)]) == n
 
+def wrong_place_correct_color_b(guess, i):
+  return And(S[i] != get_list(guess)[i], Or([And(S[j] == get_list(guess)[i], get_list(guess)[i] != get_list(guess)[j]) for j in range(N)]))
+    
+def wrong_place_correct_color_a(guess, i):
+  return And(S[i] != get_list(guess)[i], Or([S[j] == get_list(guess)[i] for j in range(N)]))
+    
+#partially correct colors a:
+def partially_correct_a(guess, n):
+  return Sum([If(wrong_place_correct_color_a(guess, i), 1, 0) for i in range(N)]) == n
 
-#partially correct colors:
-def partially_correct(guess: list[int], n: int):
-  return Sum([If(Or([And(k != i, S[k] == get_list(guess)[i]) for k in range(N)]), 1, 0) for i in range(N)]) == n
-  #return Or([And(len(I) == n, And([Or([And(k != i, S[k] == get_list(guess)[i]) for k in range(N)]) for i in I]))
-  #              for I in powerset([n for n in range(N)])])
+#helper function for part b
+def count_in_list(value, lst):
+    return Sum([If(x == value, 1, 0) for x in lst])
 
-#all_guesses_correct = And([correct(guess, number_correct(guess)) for guess in guesses])
-#solve(And(no_duplicates,S_constraint,all_guesses_correct))
+#partially correct colors b:
+def partially_correct_b(guess, n):
+  #for part b we need a different sum:
+  m = Sum([If(wrong_place_correct_color_b(guess, i), 1, 0) for i in range(N)])
+
+  max = n
+  for i in set(get_list(guess)):
+      count = count_in_list(i, get_list(guess))
+      max += If(count > 1, count - 1, 0)
+
+  return And(m >= n, m <= max)
+
 
 #we combine all guesses together
-all_guesses = And([And(correct(guess, number_correct(guess)), partially_correct(guess,number_partial(guess))) for guess in guesses])
+all_guesses_a = And([And(correct(guess, number_correct(guess)), partially_correct_a(guess,number_partial(guess))) for guess in guesses])
+all_guesses_b = And([And(correct(guess, number_correct(guess)), partially_correct_b(guess,number_partial(guess))) for guess in guesses])
 
 
 #the resulting formulas for a and b
-a = [all_guesses, no_duplicates, S_constraint]
-b = [all_guesses, S_constraint]
+a = [all_guesses_a, no_duplicates, S_constraint]
+b = [all_guesses_b, S_constraint]
 
 solver = Solver()
-solver.add(a)
+solver.add(b)
 if solver.check() == sat:
     model = solver.model()
     solution = [model[s].as_long() for s in S]
+
 
 ########## print the solution ##########
 
